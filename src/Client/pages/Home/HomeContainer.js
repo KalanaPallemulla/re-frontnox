90614cee65e926040e8c3b11e37c6463d59b7e6e
Kalana <kalanaheshan47@gmail.com>
2022-04-27 17:26:53 +0530

    add: cards

--- src/Client/pages/Home/HomeContainer.js
+++ src/Client/pages/Home/HomeContainer.js
@@ -1,13 +1,36 @@
 import React from "react";
 import WelcomeImage from "./components/WelcomeImage";
 import HomeTopic from "./components/HomeTopic";
+import EstateCard from "./components/EstateCard";
+import { ScrollMenu } from "react-horizontal-scrolling-menu";
 
 const HomeContainer = () => {
   return (
     <div className="">
       <WelcomeImage />
       <div>
-        <HomeTopic topic="For Rentals" />
+        <HomeTopic topic="For Rental" />
+        <div className="px-2 pt-4">
+          <ScrollMenu>
+            <EstateCard />
+            <EstateCard />
+            <EstateCard />
+            <EstateCard />
+            <EstateCard />
+          </ScrollMenu>
+        </div>
+      </div>
+      <div>
+        <HomeTopic topic="For Sale" />
+        <div className="px-2 pt-4">
+          <ScrollMenu>
+            <EstateCard />
+            <EstateCard />
+            <EstateCard />
+            <EstateCard />
+            <EstateCard />
+          </ScrollMenu>
+        </div>
       </div>
     </div>
   );
---
+++ src/Client/pages/Home/components/EstateCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Home from "../../../assets/Home3.jpeg";
+
+const EstateCard = () => {
+  return (
+    <div className="p-1">
+      <div
+        className="bg-red-600 h-96 w-96 flex items-end rounded-sm  border-mainDarkBrown"
+        style={{ backgroundImage: `url(${Home})` }}
+      >
+        <div className="p-4">
+          <div className="text-white text-3xl">Nugegoda</div>
+          <div className="text-white text-sm">Address</div>
+          <button className="bg-mainLightBrown text-lg AF rounded-full text-white py-1 px-2">
+            150,000,000 LKR
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EstateCard;
